68444b1fbfb94be3f32a656e05f8dc5d4cb618bb
atienosonia <sharonsonia29@gmail.com>
2023-09-07 14:11:45 +0300

    listing-shopping-items

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-# creat a function that allows users to say there names and ages
+# a function that allows users to say there names and ages
 
 def name_and_age():
     name = input('Enter your Name: ')
@@ -8,3 +8,14 @@ def name_and_age():
 
 results = name_and_age()
 print(results)
+
+# function for listing items to go back to school
+
+def back_to_school_items():
+    item_1 = input('Which setbook do you need: ')
+    item_2 = input('What sport uniform would you like? ')
+    item_3 = input('Which type of shoes do you need? ')
+    return f'The list of items I need are {item_1}, {item_2} and, {item_3}'
+
+shopping_items = back_to_school_items()
+print(shopping_items)
